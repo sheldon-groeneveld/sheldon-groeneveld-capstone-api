e39386e74e6d0f734a77031158ab436ba9777f22
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-04-21 10:18:35 -0600

    add automatic reconnect for accidental disconnect

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const io = new Server(server, {
   cors: {
     origin: "*",
   },
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 15 * 1000,
+  },
 });
 
 const PORT = process.env.PORT || 5050;
@@ -24,80 +27,83 @@ let game = {};
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-  let newUser = { id: socket.id, username: "", room: "" };
-  socket.on("create_room", (room) => {
-    if (!game[room]) {
-      game[room] = { users: [], answers: [], voteCount: 0, isOpen: true };
-    }
-  });
+  if (socket.recovered) {
+  } else {
+    let newUser = { id: socket.id, username: "", room: "" };
+    socket.on("create_room", (room) => {
+      if (!game[room]) {
+        game[room] = { users: [], answers: [], voteCount: 0, isOpen: true };
+      }
+    });
 
-  socket.on("check_room", (room) => {
-    let roomExists = `${room}` in game;
-    if (roomExists) {
-      socket.emit("room_verified", {
-        roomExists: roomExists,
-        roomIsOpen: game[room].isOpen,
-      });
-    } else {
-      socket.emit("room_verified", {
-        roomExists: roomExists,
-        roomIsOpen: false,
-      });
-    }
-  });
+    socket.on("check_room", (room) => {
+      let roomExists = `${room}` in game;
+      if (roomExists) {
+        socket.emit("room_verified", {
+          roomExists: roomExists,
+          roomIsOpen: game[room].isOpen,
+        });
+      } else {
+        socket.emit("room_verified", {
+          roomExists: roomExists,
+          roomIsOpen: false,
+        });
+      }
+    });
 
-  socket.on("join_room", ({ room, nickname }) => {
-    if (!game[room].users.includes(nickname) && game[room].users.length < 6) {
-      socket.join(room);
-      newUser = { ...newUser, username: nickname, room: room };
-      activeUsers.push(newUser);
-      game[room].users.push(nickname);
-      io.to(room).emit("lobby_list", game[room].users);
-    }
-  });
+    socket.on("join_room", ({ room, nickname }) => {
+      if (!game[room].users.includes(nickname) && game[room].users.length < 6) {
+        socket.join(room);
+        newUser = { ...newUser, username: nickname, room: room };
+        activeUsers.push(newUser);
+        game[room].users.push(nickname);
+        io.to(room).emit("lobby_list", game[room].users);
+      }
+    });
 
-  socket.on("start_game", (room) => {
-    game[room].isOpen = false;
-    io.to(room).emit("game_start");
-  });
+    socket.on("start_game", (room) => {
+      game[room].isOpen = false;
+      io.to(room).emit("game_start");
+    });
 
-  socket.on("get_users", (room) => {
-    io.to(room).emit("users_get", game[room].users);
-  });
+    socket.on("get_users", (room) => {
+      io.to(room).emit("users_get", game[room].users);
+    });
 
-  socket.on("send_answer", ({ room, payload }) => {
-    game[room].answers.push(payload);
-    if (game[room].answers.length === game[room].users.length) {
-      io.to(room).emit("recieve_answer", shuffle(game[room].answers));
-    }
-  });
+    socket.on("send_answer", ({ room, payload }) => {
+      game[room].answers.push(payload);
+      if (game[room].answers.length === game[room].users.length) {
+        io.to(room).emit("recieve_answer", shuffle(game[room].answers));
+      }
+    });
 
-  socket.on("send_vote", ({ room, nickname, vote }) => {
-    game[room].answers
-      .find((answer) => answer.answer === vote)
-      .voters.push(nickname);
-    game[room].voteCount++;
-    if (game[room].voteCount === game[room].users.length) {
-      io.to(room).emit("show_results", game[room].answers);
-    }
-  });
+    socket.on("send_vote", ({ room, nickname, vote }) => {
+      game[room].answers
+        .find((answer) => answer.answer === vote)
+        .voters.push(nickname);
+      game[room].voteCount++;
+      if (game[room].voteCount === game[room].users.length) {
+        io.to(room).emit("show_results", game[room].answers);
+      }
+    });
 
-  socket.on("reset_room", ({ room }) => {
-    game[room].answers = [];
-    game[room].voteCount = 0;
-    io.to(room).emit("room_reset", game[room].answers);
-  });
+    socket.on("reset_room", ({ room }) => {
+      game[room].answers = [];
+      game[room].voteCount = 0;
+      io.to(room).emit("room_reset", game[room].answers);
+    });
 
-  socket.on("disconnect", () => {
-    let disconnectedUser = activeUsers.find((user) => user.id === socket.id);
-    activeUsers = activeUsers.filter((user) => user.id !== socket.id);
-    if (disconnectedUser && disconnectedUser.room) {
-      game[disconnectedUser.room].users.splice(
-        game[disconnectedUser.room].users.indexOf(disconnectedUser.username),
-        1
-      );
-    }
-  });
+    socket.on("disconnect", () => {
+      let disconnectedUser = activeUsers.find((user) => user.id === socket.id);
+      activeUsers = activeUsers.filter((user) => user.id !== socket.id);
+      if (disconnectedUser && disconnectedUser.room) {
+        game[disconnectedUser.room].users.splice(
+          game[disconnectedUser.room].users.indexOf(disconnectedUser.username),
+          1
+        );
+      }
+    });
+  }
 });
 
 server.listen(PORT, () => {
